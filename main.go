2dd62c1d188fd112d1db505cb309d2d7fc583119
michel novellino <michelnovellino.programador@gmail.com>
2020-04-27 21:45:15 -0400

    initial

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gofiber/cors"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Settings.Prefork = true
+	app.Settings.CaseSensitive = true
+	app.Settings.StrictRouting = true
+	app.Settings.ServerHeader = "Fiberabid"
+
+	app.Static("/static", "./public", fiber.Static{
+		Compress:  true,
+		ByteRange: true,
+		Browse:    true,
+		Index:     "lol.txt",
+	})
+	app.Use(cors.New())
+	app.Use(logger.New())
+
+	app.Get("/greetings", func(c *fiber.Ctx) {
+		c.Send("Welcome!")
+	})
+
+	v1 := app.Group("/v1")
+	v1.Get("/user/*", func(c *fiber.Ctx) {
+		c.Send("API path: " + c.Params("*"))
+	})
+	app.Use(func(c *fiber.Ctx) {
+		c.SendStatus(404) // => 404 "Not Found"
+		c.Send("This is a dummy route")
+	})
+	app.Listen(3000)
+}
